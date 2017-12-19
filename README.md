f9377be7af02c52302a73c686175d76526b4bb2d
Vlad Zadvorny <vladzadvorny@icloud.com>
2017-12-19 11:40:11 +0300

    ejs and prettier

--- README.md
+++ README.md
@@ -5,3 +5,12 @@
 Eslint: [https://www.npmjs.com/package/eslint-plugin-node](https://www.npmjs.com/package/eslint-plugin-node)
 
 Express: [https://expressjs.com/en/starter/hello-world.html](https://expressjs.com/en/starter/hello-world.html)
+
+## Видео 2
+
+Pug [https://github.com/pugjs/pug](https://github.com/pugjs/pug)
+
+Ejs [http://ejs.co/](http://ejs.co/)
+
+Prettier [https://github.com/prettier/prettier](https://github.com/prettier/prettier)
+
--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const arr = ['hello', 'world', 'test'];
+
+app.get('/', (req, res) => res.render('index', { arr: arr }));
+
+app.get('/create', (req, res) => res.render('create'));
+app.post('/create', (req, res) => {
+  arr.push(req.body.text);
+  res.redirect('/');
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
