f9a55be346e84d3cac0748c39337a064d54d9d91
Vlad Zadvorny <vladzadvorny@icloud.com>
2018-06-29 09:03:43 +0300

    quality im set 10

--- utils/diskStorage.js
+++ utils/diskStorage.js
@@ -43,7 +43,7 @@ DiskStorage.prototype._handleFile = function _handleFile(req, file, cb) {
       // file.stream.pipe(risizer).pipe(outStream);
       gm(file.stream)
         .resize('1024', '768', '>')
-        .quality(30)
+        .quality(10)
         // eslint-disable-next-line
         .stream(function(err, stdout, stderr) {
           stdout.pipe(outStream);
